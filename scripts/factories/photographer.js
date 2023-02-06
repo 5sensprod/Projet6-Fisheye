a3f2e0fbd48bc0996608704bd3eba278454a7461
chris <5sensprod@gmail.com>
2023-02-06 14:56:45 +0100

    Récupére country ,city, tagline, price

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,9 +9,12 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const p = document.createElement( 'p' );
+        p.textContent = `${city}, ${country} - ${tagline} - ${price}€`;
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(p);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, country, tagline, price, getUserCardDOM }
 }
