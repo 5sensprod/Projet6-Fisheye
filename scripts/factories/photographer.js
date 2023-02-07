24d566ea678e1d5a28982ec98bcb39a74ae8b50b
chris <5sensprod@gmail.com>
2023-02-07 13:08:29 +0100

    Amélioration accessibilité DOM/CSS accueil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -5,14 +5,28 @@ function photographerFactory(data) {
 
     function getUserCardDOM() {
         const article = document.createElement('article');
+        article.setAttribute("role", "region");
+
+        const figure = document.createElement('figure');
+        figure.classList.add('card_portrait');
+        figure.setAttribute("tabindex", "0");
+        figure.addEventListener("keydown", function (event) {
+            if (event.key === "Enter" || event.key === " ") {
+                window.location.href = `photographer.html?id=${data.id}`;
+            }
+        });
+
+        figure.addEventListener("click", function () {
+            window.location.href = `photographer.html?id=${data.id}&name=${data.name}`;
+        });
+
         const img = document.createElement('img');
         img.setAttribute("src", picture);
-        img.addEventListener("click", function() {
-            // console.log("Image cliquée");
-            window.location.href = `photographer.html?id=${data.id}`;
-          });
+        img.setAttribute("alt", `Portrait de ${name}`);
+
         const h2 = document.createElement('h2');
         h2.textContent = name;
+
         const descContainer = document.createElement('div');
         descContainer.classList.add('card_desc-container');
         const location = document.createElement('p');
@@ -22,15 +36,17 @@ function photographerFactory(data) {
         taglineElement.textContent = tagline;
         taglineElement.classList.add('card_tagline');
         const priceElement = document.createElement('p');
-        priceElement.textContent = `${price}€`;
+        priceElement.textContent = `${price} € / jour`;
         priceElement.classList.add('card_price');
         descContainer.appendChild(location);
         descContainer.appendChild(taglineElement);
         descContainer.appendChild(priceElement);
-        article.appendChild(img);
-        article.appendChild(h2);
+
+        figure.appendChild(img);
+        figure.appendChild(h2);
+        article.appendChild(figure);
         article.appendChild(descContainer);
-        return (article);
+        return article;
     }
     return { name, picture, city, country, tagline, price, id, getUserCardDOM }
 }
