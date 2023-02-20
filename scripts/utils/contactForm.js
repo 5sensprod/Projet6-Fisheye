b89ca2cc41e9187d3de875791b018b6a02ed9e6c
chris <5sensprod@gmail.com>
2023-02-20 18:18:02 +0100

    refactorisation modalForm

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,6 +1,5 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
-    modal.style.display = "block";
     const url = new URL(window.location.href);
     const id = url.searchParams.get("id");
   
@@ -8,42 +7,49 @@ function displayModal() {
       .then(response => response.json())
       .then(data => {
         const photographer = data.photographers.find(p => p.id == id);
-  
         const photographerNameDiv = document.getElementById("photographer-name");
         photographerNameDiv.textContent = photographer.name;
-  
-        // Met le focus sur le premier élément du formulaire
         document.querySelector("#lastname").focus();
-  
-        // Gère l'accessibilité de la modale
         const modalContainer = document.getElementById("contact_modal");
         const photographerName = photographer.name;
         modalContainer.setAttribute("role", "dialog");
         modalContainer.setAttribute("aria-label", `Formulaire pour contacter ${photographerName}`);
+        modalContainer.setAttribute('aria-hidden', 'true');
       });
+  
+    const pageContent = document.querySelector('.page-content');
+    pageContent.classList.add('modal-open');
+    modal.style.display = "block";
   }
   
   function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
+    modal.removeAttribute('aria-hidden');
+    const pageContent = document.querySelector('.page-content');
+    pageContent.classList.remove('modal-open');
+  }
+  
+  function submitForm(event) {
+    event.preventDefault();
+    const lastname = document.querySelector("#lastname").value;
+    const firstname = document.querySelector("#firstname").value;
+    const email = document.querySelector("#email").value;
+    const message = document.querySelector("#message").value;
+    console.log("Nom :", lastname);
+    console.log("Prénom :", firstname);
+    console.log("Email :", email);
+    console.log("Message :", message);
+    form.reset();
+    closeModal();
   }
   
   const form = document.querySelector("form");
   const submitButton = document.querySelector(".submit_button");
   
-  submitButton.addEventListener("click", (event) => {
-    event.preventDefault();
-  
-    const lastname = document.querySelector("#lastname").value;
-    const firstname = document.querySelector("#firstname").value;
-    const email = document.querySelector("#email").value;
-    const message = document.querySelector("#message").value;
-  
-    console.log("Nom :", lastname);
-    console.log("Prénom :", firstname);
-    console.log("Email :", email);
-    console.log("Message :", message);
-  
-    form.reset();
-    closeModal();
+  submitButton.addEventListener("click", submitForm);
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+      closeModal();
+    }
   });
