4678abc136c861ca093b2c2a5c87325367fb50f4
chris <5sensprod@gmail.com>
2023-02-25 15:59:27 +0100

    nettoyage media.js

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -22,13 +22,13 @@ function createMedia(id, title, image, likes, date, price, photographerId, type)
   media.render = function () {
     const mediaItem = document.createElement("article");
     mediaItem.classList.add("media-item");
-
+  
     const mediaLink = createMediaLink(this);
-
+  
     let mediaContent = this.type === imageType
       ? document.createElement("img")
       : document.createElement("video");
-
+  
     if (this.type === imageType) {
       mediaContent.classList.add("media-img");
       mediaContent.setAttribute("src", `${mediaUrl}/${this.photographerId}/${this.image}`);
@@ -37,47 +37,47 @@ function createMedia(id, title, image, likes, date, price, photographerId, type)
       mediaContent.classList.add("media-video");
       mediaContent.setAttribute("src", `${mediaUrl}/${this.photographerId}/${this.image}`);
       mediaContent.setAttribute("alt", this.title);
-
+  
       const description = document.createElement("source");
       description.setAttribute("src", `${mediaUrl}/${this.photographerId}/${this.description}`);
       description.setAttribute("type", "audio/mpeg");
       mediaContent.appendChild(description);
     }
-
+  
     mediaLink.appendChild(mediaContent);
     mediaItem.appendChild(mediaLink);
-
+  
     if (this.type === videoType) {
       mediaContent.addEventListener('click', function (e) {
         e.preventDefault();
         showLightbox(media);
       });
     }
-
+  
     const mediaInfo = document.createElement("div");
     mediaInfo.classList.add("media-info");
-
+  
     const mediaTitle = document.createElement("h2");
     mediaTitle.classList.add("media-title");
     mediaTitle.textContent = this.title;
-
+  
     const mediaLikes = document.createElement("div");
     mediaLikes.classList.add("media-likes");
-
+  
     const likesIcon = document.createElement("i");
     likesIcon.classList.add("fas", "fa-heart");
     let isLiked = false;
-
+  
     const likesCount = document.createElement("span");
     likesCount.classList.add("likes-count");
     likesCount.textContent = this.likes;
-
+  
     mediaLikes.appendChild(likesIcon);
     mediaLikes.appendChild(likesCount);
-
+  
     mediaInfo.appendChild(mediaTitle);
     mediaInfo.appendChild(mediaLikes);
-
+  
     likesIcon.addEventListener("click", function () {
       isLiked ? (
         likesCount.textContent = parseInt(likesCount.textContent) - 1,
@@ -93,13 +93,14 @@ function createMedia(id, title, image, likes, date, price, photographerId, type)
         totalLikesEl.innerHTML = `${totalLikes} <i class="fas fa-heart"></i>`
       );
     });
-
+  
     mediaItem.appendChild(mediaInfo);
-
+  
     return mediaItem;
   };
   return media;
 }
+
 function createMediaLink(media) {
   const mediaLink = document.createElement("a");
   mediaLink.classList.add("media-link");
@@ -113,13 +114,8 @@ function createMediaLink(media) {
     e.preventDefault(); // empêche le comportement par défaut du lien
     showLightbox(media);
   });
-
   return mediaLink;
 }
-
-
-
-
 
 //Calcule le nombre total de likes pour tous les médias
 function getTotalLikes(media) {
@@ -131,7 +127,6 @@ function getTotalLikes(media) {
   });
   return totalLikes;
 }
-
 
 //Crée un média à partir des données récupérées en fonction du type de media
 function createMediaFromData(data, photographerId) {
@@ -151,8 +146,6 @@ function createMediaFromType(data, photographerId, type) {
     : createMedia(data.id, data.title, data.image, data.likes, data.date, data.price, photographerId, imageType);
 }
 
-
-
 //Récupère les données de médias pour le photographe actuel et les affiche
 let media = [];
 
@@ -164,17 +157,19 @@ function renderMedia() {
   });
 }
 
+
+//Fonction de tri des médias
 function sortMedia() {
+  const sortFunctions = {
+    popularity: (a, b) => b.likes - a.likes,
+    date: (a, b) => Date.parse(a.date) - Date.parse(b.date),
+    title: (a, b) => a.title.localeCompare(b.title)
+  };
+
   const sortBy = sortBySelect.value;
+  const sortFunction = sortFunctions[sortBy];
 
-  if (sortBy === "popularity") {
-    media.sort((a, b) => b.likes - a.likes);
-  } else if (sortBy === "date") {
-    media.sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
-  } else if (sortBy === "title") {
-    media.sort((a, b) => a.title.localeCompare(b.title));
-  }
-
+  media.sort(sortFunction);
   renderMedia();
 }
 
